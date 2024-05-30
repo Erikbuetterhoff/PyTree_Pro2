b03000d764fe899894fb236ebd53b5d64bae4d77
Jielty123 <jelte.wierper@gmail.com>
2024-05-30 10:01:54 +0200

    imported actions as folder not single files

--- test_bt_pkg/test_bt_pkg/bt_test_sub.py
+++ test_bt_pkg/test_bt_pkg/bt_test_sub.py
@@ -7,7 +7,7 @@ import sys
 import py_trees.console as console
 from std_msgs.msg import Bool
 
-from action_pkg.action import Empty
+import action_pkg.action as Actions
 
 
 
@@ -15,12 +15,12 @@ def tutorial_create_root() -> py_trees.behaviour.Behaviour:
     
     root = py_trees.composites.Sequence(name="Sequece Dronecheck")
 
-    drone_not_ok = py_trees_ros.actions.ActionClient(       #direkt aus tutorial kopiert
+    drone_not_ok = py_trees_ros.actions.ActionClient(
         name="Return Home",
-        action_type=Empty,
+        action_type=Actions.Empty,
         action_name="empty_action",
-        action_goal=Empty.Goal(),
-        generate_feedback_message=lambda msg: Empty.Feedback()
+        action_goal=Actions.Empty.Goal(),
+        generate_feedback_message=lambda msg: Actions.Empty.Feedback()
     )
     drone_ok = py_trees_ros.subscribers.CheckData(name="Drohne okay?", topic_name="bt_test_topic",topic_type=Bool, variable_name="data", expected_value= True, fail_if_bad_comparison= True, qos_profile=2, clearing_policy=2)
     
